c2262f04f778c2eabee4c3cecf96ff8e79f6c0a7
chengwill97 <chengwill97@gmail.com>
2016-11-03 20:52:58 -0400

    Commit and Sync Master

--- client/js/welcome-page/welcome-page.component.js
+++ client/js/welcome-page/welcome-page.component.js
@@ -1,10 +1,13 @@
-//welcome-page.component.js
-//Author: Jeremy Savarin
+// welcome-page.component.js
+// Author: Jeremy Savarin
 
 angular.module("isn-server.welcome-page")
     .component("isnWelcomePage", {
         templateUrl: "js/welcome-page/welcome-page.component.html",
-        controller: WelcomePageController
+        controller: WelcomePageController,
     });
 
-function WelcomePageController() { }
+function WelcomePageController() 
+{ 
+
+}
