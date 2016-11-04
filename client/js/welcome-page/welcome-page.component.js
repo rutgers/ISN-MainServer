db627ab5a78446b06050f0750190b1f34caabe40
Luckysena <mg1250@scarletmail.rutgers.edu>
2016-11-03 21:02:41 -0400

    Inject auth service to welcome page controller

--- client/js/welcome-page/welcome-page.component.js
+++ client/js/welcome-page/welcome-page.component.js
@@ -7,4 +7,64 @@ angular.module("isn-server.welcome-page")
         controller: WelcomePageController
     });
 
-function WelcomePageController() { }
+WelcomePageController.$inject = ["authService"];
+
+
+function WelcomePageController(authService) {
+    var vm = this;
+    var toggle = true;
+
+	vm.loginEmail = "";
+	vm.loginPassword = "";
+
+	vm.signupEmail = "";
+	vm.signupPassword = "";
+	vm.confirmPassword = "";
+
+	//function for toggle, one for login, one for sign in
+
+	vm.toggleLogin = function() {
+		vm.toggle = !vm.toggle;
+	}
+
+	// login the user
+	vm.loginUser = function(){
+		authService.$signInWithEmailAndPassword(vm.loginEmail,vm.loginPassword)
+		.then(function(userData){
+			console.log("Success");
+			vm.loginEmail = "";
+			vm.loginPassword = "";
+
+		}).catch(function(error){
+			console.log(error)
+		});
+	}
+
+
+
+	//sign up for the user
+	vm.signupUser = function(){
+		// accounts for when signup password doesnt match the confirm password
+		if(vm.signupPassword!==vm.confirmPassword){
+			console.log("Passwords don't match");
+			return;
+		}
+
+
+		authService.$createUserWithEmailAndPassword(vm.signupEmail,vm.signupPassword)
+		.then(function(userData){
+			console.log("Success");
+			vm.signUpEmail = "";
+			vm.signUpPassword = "";
+			vm.confirmPassword = "";
+
+		}).catch(function(error){
+			console.log(error)
+		});
+	}
+
+
+	
+
+}
+
